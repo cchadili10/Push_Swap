892ef4ea2d8af4664d7f7832b7f1f54f0ed1cf8f
Hamza chadili <chadilisafi@gmail.com>
2024-03-24 23:00:05 +0000

    push_swap

--- Mandatory/push_swap.c
+++ Mandatory/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: hchadili <hchadili@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 17:40:33 by hchadili          #+#    #+#             */
-/*   Updated: 2024/03/24 03:07:27 by hchadili         ###   ########.fr       */
+/*   Updated: 2024/03/24 22:58:50 by hchadili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@ void printList(t_node *node, char *s)
 {
 	while (node != NULL)
 	{
-		printf("STACK %s -->  Index %d: Data %d\n",s, node->index, node->data);
+		printf("STACK %s -->  Index %d: Data %d\n", s, node->index, node->data);
 		node = node->next;
 	}
 }
@@ -29,9 +29,9 @@ t_node *createNode(int data, int index)
 	return newNode;
 }
 
-void insertEnd(t_node **head, int data,int index)
+void insertEnd(t_node **head, int data, int index)
 {
-	t_node *newNode = createNode(data , index);
+	t_node *newNode = createNode(data, index);
 	if (*head == NULL)
 	{
 		*head = newNode;
@@ -45,7 +45,6 @@ void insertEnd(t_node **head, int data,int index)
 	temp->next = newNode;
 }
 
-
 int get_index(t_node *node, int data)
 {
 	int x;
@@ -53,30 +52,30 @@ int get_index(t_node *node, int data)
 	x = 0;
 	while (node != NULL)
 	{
-		if(data > node->data)
+		if (data > node->data)
 			x++;
 		node = node->next;
 	}
 	return (x);
 }
-void ft_error(char *s,t_node *node)
+void ft_error(char *s, t_node *node)
 {
-	printf("%s",s);
+	printf("%s", s);
 	free(node);
 	exit(0);
 }
 void index_list(t_node *node)
 {
 	t_node *temp1;
-	
+
 	temp1 = node;
 	while (node != NULL)
 	{
-		node->index = get_index(temp1,node->data);
+		node->index = get_index(temp1, node->data);
 		node = node->next;
 	}
 }
-int	ft_isdigit( int arg )
+int ft_isdigit(int arg)
 {
 	if (arg >= '0' && arg <= '9')
 		return (1);
@@ -85,28 +84,28 @@ int	ft_isdigit( int arg )
 int is_number(char *s)
 {
 	int x = 0;
-	
-	if((s[x] != '+' && s[x] != '-') && (s[x] < 48 || s[x] > 57))
-	    return 1;
+
+	if ((s[x] != '+' && s[x] != '-') && (s[x] < 48 || s[x] > 57))
+		return 1;
 	x++;
 	while (s[x])
 	{
-		if(s[x] < 48 || s[x] > 57 )
+		if (s[x] < 48 || s[x] > 57)
 			return 1;
 		x++;
 	}
 	return 0;
 }
-int	ft_repeted_number(t_node *a,int i)
+int ft_repeted_number(t_node *a, int i)
 {
 	t_node *tmp = a;
 	static int x;
 	int ckeck = 0;
-	if(i == 0)
+	if (i == 0)
 		return 1;
 	while (tmp)
 	{
-		if(tmp->index == x)
+		if (tmp->index == x)
 		{
 			ckeck = 1;
 			break;
@@ -114,11 +113,25 @@ int	ft_repeted_number(t_node *a,int i)
 		tmp = tmp->next;
 	}
 	x++;
-	if(ckeck == 1)
+	if (ckeck == 1)
 	{
-		return ft_repeted_number(a,i-1);
+		return ft_repeted_number(a, i - 1);
 	}
 	return 0;
+}
+int is_lost_sort(t_node *node, int size)
+{
+	int x;
+
+	x = 0;
+	while (size)
+	{
+		if (node->index != x)
+			return 0;
+		size--;
+		x++;
+	}
+	return 1;
 }
 int main(int argc, char *argv[])
 {
@@ -126,45 +139,46 @@ int main(int argc, char *argv[])
 	t_node *b = NULL;
 	char **array;
 	int i;
+	int j;
+	int counter;
 	i = 0;
+	j = 0;
+	counter = 0;
 	if (argc == 1)
 		return 0;
-	if (argc == 2)
-	{
-		array = ft_split(argv[1], ' ');
-		while (array[i])
-		{
-			if(is_number(array[i]))
-				ft_error("put valid number",a);
-			insertEnd(&a, ft_atoi(array[i]),0);
-			free(array[i]);
-			i++;
-		}
-		free(array);
-	}
 	else
 	{
 		i = 1;
 		while (argv[i])
 		{
-			if(is_number(argv[i]))
-				ft_error("put valid number",a);
-			insertEnd(&a, ft_atoi(argv[i]),0);
+			array = ft_split(argv[i], ' ');
+			while (array[j])
+			{
+				if (is_number(array[j]))
+					ft_error("Error", a);
+				insertEnd(&a, ft_atoi(array[j]), 0);
+				free(array[j]);
+				j++;
+				counter++;
+			}
+			j = 0;
+			free(array);
 			i++;
 		}
-		i--;
 	}
-	
 	index_list(a);
-	if(ft_repeted_number(a,i) == 0)
-		ft_error("repeated number",a);
-	if(i > 50)
+	// printList(a,"A");
+	if (ft_repeted_number(a, counter) == 0)
+		ft_error("Error", a);
+	if(is_lost_sort(a,counter))
+		return 0;
+	if (counter > 50)
 	{
-		split_into_chunks(&a,&b,(i));
-		sort_ink(&b,&a, i);
+		split_into_chunks(&a, &b, counter);
+		sort_ink(&b, &a, counter);
 	}
 	else
-		sort_link(&a,&b, i);
+		sort_link(&a, &b, counter);
 	// system("leaks push_swap");
 	return 0;
 }
